85a0b5fba79e2280fa1027ffe28f8bae510374bf
xiaxichen <xiaxichen1@163.com>
2020-09-11 19:20:07 +0800

    change fix bug for ConfigParser parser

--- base_client.py
+++ base_client.py
@@ -1,12 +1,15 @@
+import os
 from configparser import ConfigParser
 
 import pymysql
+curpath = os.path.dirname(os.path.realpath(__file__))
+cfgpath = os.path.join(curpath, "config.ini")
 
-config = ConfigParser.ConfigParser()
-config.read('config.ini')
+config = ConfigParser()
+config.read(cfgpath, encoding="utf-8")
 
-mysql_list = config.options("MYSQL_SETTING")
+mysqlOption = dict(config.items("MYSQL_SETTING"))
 
-connect = pymysql.connect(host=mysql_list.get("HOST_NAME"), port=mysql_list.get("HOST_NAME"),
-                          user=mysql_list.get("USER"), password=mysql_list.get("PASSWORD"),
-                          db=mysql_list.get("DB_NAME"), charset=mysql_list.get("CHARSET"))
+connect = pymysql.connect(host=mysqlOption.get("host_name"), port=int(mysqlOption.get("port")),
+                          user=mysqlOption.get("user"), password=mysqlOption.get("password"),
+                          db=mysqlOption.get("db_name"), charset=mysqlOption.get("charset"))
